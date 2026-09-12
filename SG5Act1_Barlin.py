966f0d2be044901f3a0b8cf23d32fcafd35198aa
Maxaqb <mqbarlin@carc.pshs.edu.ph>
2026-09-12 19:48:32 +0800

    Add files via upload

--- SG5Act1_Barlin.py
+++ SG5Act1_Barlin.py
@@ -1,3 +1,5 @@
+
+# Defining the main class, attrubutes, and methods
 class Hero:
   def __init__(self, name, health, attack):
     self.name = name
@@ -6,54 +8,28 @@ class Hero:
 
   def take_damage(self, damage):
     self.health -= damage
-    if self.health < 0:
-      self.health = 0
 
-  def attack(self, damage):
-    return self.attack
 
+# Defining the heros
 arthur = Hero("Arthur", 100, 15)
 morgana = Hero("Morgana", 100, 10)
 
 
-#Simulating Arthur taking 10 damage 
+# Simulating Arthur taking 10 damage 
 for i in range(1):
   arthur.take_damage(morgana.attack)
   print(f"Arthur's health after taking 10 damage: {arthur.health}")
 
-# Showing both their HPs to see that Morgana is still at full health
-
+# Print both their HPs to see that Morgana is still at full health
+print()
+print("Hero's health left:")
+print(f"Arthur's Health: {arthur.health}")
+print(f"Morgana's Health: {morgana.health}")
 print()
 
-print("Hero's hp left:")
-print(f"Arthur: {arthur.health}")
-print(f"Morgana: {morgana.health}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Check if anyone is anyone is at full health
+if arthur.health == 100:
+  print("Arthur is at full health!")
+if morgana.health == 100:
+  print("Morgana is at full health!")
+  
